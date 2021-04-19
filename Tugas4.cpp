baeafad3a104b88e8cc20a0471aeb2bd87c14443
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-04-19 10:19:31 +0700

    Add files via upload

---
+++ Tugas4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int X = 30;
+	cout << "Nilai Awal X : " << X << endl;
+	cout << "Nilai Kedua X : " << --X << endl;
+	cout << "Nilai Ketiga X : " << ++++X << endl;
+	return 0;
+}
